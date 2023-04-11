cf986092f3444445d35b8642bd68190ec2a62004
Thomas L <thomas.lombard@interhaptics.com>
2023-04-11 04:44:11 +0200

    new version

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const { app, BrowserWindow, Notification } = require('electron');
+const { app, BrowserWindow, Notification, autoUpdater } = require('electron');
 const path = require('path');
+const server = 'https://github.com/TLRazer/Auto-Update-test'
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
 
+autoUpdater.setFeedURL({ url })
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000)
+
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -27,9 +37,30 @@ const createWindow = () => {
     app.on('activate', () => {
       if (BrowserWindow.getAllWindows().length === 0) createWindow()
     });
+
   }).then(showNotification);
 
 
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
   });
+
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail:
+        'A new version has been downloaded. Restart the application to apply the updates.',
+    }
+  
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    })
+  })
+
+  autoUpdater.on('error', (message) => {
+    console.error('There was a problem updating the application')
+    console.error(message)
+  })
