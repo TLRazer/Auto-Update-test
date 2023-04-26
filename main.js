65f2bdbc1e9656b66e29165251c28b85246b3e0b
Thomas L <thomas.lombard@interhaptics.com>
2023-04-27 01:42:22 +0200

    1.0.14

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ Object.defineProperty(app, 'isPackaged', {
 const APP_PATH = app.getAppPath();
 
 autoUpdater.updateConfigPath = path.join(APP_PATH, 'resources\\app-update.yml');
-autoUpdater.addAuthHeader("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");
+autoUpdater.addAuthHeader("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");
 
 autoUpdater.logger = require("electron-log")
 autoUpdater.setFeedURL({
